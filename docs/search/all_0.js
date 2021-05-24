f74f84b49eb809cbe939a03525c9805bd3a339ce
Brian <bdreece@mtu.edu>
2021-05-24 17:20:30 -0400

    Added documentation

---
+++ docs/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['data_2dstructures_0',['data-structures',['../md_README.html',1,'']]]
+];
---
+++ docs/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['data_2dstructures_1',['data-structures',['../md_README.html',1,'']]]
+];
