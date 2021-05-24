d44030d981fd876d985cb0792dd3c426650b7f6b
Brian Reece <bdreece@mtu.edu>
2021-05-24 21:08:36 +0000

    Began writing stubs for HashMap

---
+++ src/hashmap.c
@@ -0,0 +1,64 @@
+//! \file hashmap.c
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hashmap.h"
+#include "list.h"
+#include "map.h"
+
+static int hashmap_rehash(HashMap_t *map);
+
+int hashmap_init(HashMap_t *map, size_t key_size, size_t val_size, float load_factor, int (*cmp)(void *, void *))
+{
+  // TODO: Initialize HashMap
+  return 0;
+}
+
+int hashmap_deinit(HashMap_t *map)
+{
+  // TODO: Deinitialize HashMap
+  return 0;
+}
+
+int hashmap_deinit(HashMap_t *map)
+{
+  // TODO: Deinitialize HashMap
+  return 0;
+}
+
+int hashmap_deinit(HashMap_t *map)
+{
+  // TODO: Deinitialize HashMap
+  return 0;
+}
+
+
+int hashmap_deinit(HashMap_t *map)
+{
+  // TODO: Deinitialize HashMap
+  return 0;
+}
+
+int hashmap_deinit(HashMap_t *map)
+{
+  // TODO: Deinitialize HashMap
+  return 0;
+}
+
+int hashmap_deinit(HashMap_t *map)
+{
+  // TODO: Deinitialize HashMap
+  return 0;
+}
+
+
+
+#ifdef __cplusplus
+}
+#endif
