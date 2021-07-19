da75f4f884bee72f1ce5f100a67ba1ac65ef9bc0
Brian Reece <bdreece@mtu.edu>
2021-07-19 18:22:14 -0400

    Renamed buffer structure and updated implementation and documentation for vla

--- inc/vla.h
+++ inc/vla.h
@@ -12,6 +12,10 @@ extern "C" {
 
 #include <stddef.h>
 
+/*! \ref vla_errno
+ *  \brief VLA error codes.
+ *  \details These are error codes returned by the VLA functions.
+ */
 #define VLA_SUCCESS 0
 #define VLA_FAILURE -1
 #define VLA_ARRAY_SIZE_MISMATCH -2
@@ -21,39 +25,92 @@ extern "C" {
 
 //! \brief VLA struct definition.
 typedef struct vla {
-    unsigned long size;
-    unsigned long capacity;
-    size_t element_size;
-    void **elements;
+    unsigned long size; //!< VLA size.
+    unsigned long capacity; //!< VLA capacity.
+    size_t element_size; //!< VLA element size.
+    void **elements; //!< VLA elements.
 } vla_t;
-//! \brief VLA creation function.
-int vla_init(struct vla *vla, size_t element_size, unsigned long initial_size);
 
-//! \brief VLA push function.
+/*! \brief VLA creation function.
+ *  \details This function creates a VLA of a given size and element size.
+ *  \param[in] vla VLA to create.
+ *  \param[in] size VLA size.
+ *  \param[in] element_size VLA element size.
+ *  \param[in] initial_capacity VLA initial capacity.  
+ *  \return Zero on success, non-zero on failure.
+ */
+int vla_init(struct vla *vla, size_t element_size, unsigned long initial_capacity);
+
+/*! \brief VLA push function.
+ *  \details This function pushes an element onto the VLA.
+ *  \param[in] vla VLA to push to.
+ *  \param[in] element Element to push.
+ *  \return Zero on success, non-zero on failure.
+ */
 int vla_push(struct vla *vla, void *element);
 
-//! \brief VLA pop function.
+/*! \brief VLA pop function.
+ *  \details This function pops an element from the VLA.
+ *  \param[in] vla VLA to pop from.
+ *  \param[out] element Element to pop.
+ *  \return Zero on success, non-zero on failure.
+ */
 int vla_pop(struct vla *vla, void *element);
 
-//! \brief VLA get function.
-int vla_get(struct vla *vla, unsigned long i, void *element);
+/*! \brief VLA get function.
+ *  \details This function gets an element from the VLA.
+ *  \param[in] vla VLA to get from.
+ *  \param[in] index Index of element to get.
+ *  \param[out] element Element to get.
+ *  \return Zero on success, non-zero on failure.
+ */
+int vla_get(struct vla *vla, unsigned long index, void *element);
 
-//! \brief VLA set function.
-int vla_set(struct vla *vla, unsigned long i, void *element);
+/*! \brief VLA set function.
+ *  \details This function sets an element in the VLA.
+ *  \param[in] vla VLA to set in.
+ *  \param[in] index Index of element to set.
+ *  \param[in] element Element to set.
+ *  \return Zero on success, non-zero on failure.
+ */
+int vla_set(struct vla *vla, unsigned long index, void *element);
 
-//! \brief VLA insert function.
+/*! \brief VLA insert function.
+ *  \details This function inserts an element into the VLA.
+ *  \param[in] vla VLA to insert into.
+ *  \param[in] index Index to insert at.
+ *  \param[in] element Element to insert.
+ *  \return Zero on success, non-zero on failure.
+ */
 int vla_insert(struct vla *vla, void *element, unsigned long index);
 
-//! \brief VLA delete function.
+/*! \brief VLA delete function.
+ *  \details This function deletes an element from the VLA.
+ *  \param[in] vla VLA to delete from.
+ *  \param[in] index Index of element to delete.
+ *  \return Zero on success, non-zero on failure.
+ */
 int vla_delete(struct vla *vla, unsigned long i);
 
-//! \brief VLA clear function.
+/*! \brief VLA clear function.
+ *  \details This function clears the VLA.
+ *  \param[in] vla VLA to clear.
+ *  \return Zero on success, non-zero on failure.
+ */
 int vla_clear(struct vla *vla);
 
-//! \brief VLA size function.
+/*! \brief VLA size function.
+ *  \details This function returns the VLA size.
+ *  \param[in] vla VLA to get size of.
+ *  \return VLA size.
+ */
 unsigned long vla_size(struct vla *vla);
 
-//! \brief VLA capacity function.
+/*! \brief VLA capacity function.
+ *  \details This function returns the VLA capacity.
+ *  \param[in] vla VLA to get capacity of.
+ *  \return VLA capacity.
+ */
 unsigned long vla_capacity(struct vla *vla);
 
 #ifdef __cplusplus
--- src/vla.c
+++ src/vla.c
@@ -7,104 +7,138 @@
 
 #include "vla.h"
 
-static int vla_resize(vla_t *vla, unsigned long size) {
-    void *p = realloc(vla->elements, vla->element_size * size);
-    if (p == NULL) {
+/*! \brief Resize a variable length array.
+ *  \details The new size is calculated by multiplying the element size by the new capacity
+ *  \param[in] vla Pointer to the variable length array.
+ *  \param[in] capacity The new capacity of the array.
+ *  \returns Zero on success, non-zero on failure.
+ */
+static int vla_resize(vla_t *vla, unsigned long capacity)
+{
+    void *p = realloc(vla->elements, vla->element_size * capacity);
+
+    if (p == NULL)
         return VLA_FAILURE;
-    }
+
     vla->elements = p;
-    vla->capacity = size;
+    vla->capacity = capacity;
+
     return VLA_SUCCESS;
 }
 
-unsigned long vla_size(vla_t *vla) {
+unsigned long vla_size(vla_t *vla)
+{
     return vla->size;
 }
 
-unsigned long vla_capacity(vla_t *vla) {
+unsigned long vla_capacity(vla_t *vla)
+{
     return vla->capacity;
 }
 
-int vla_init(vla_t *vla, size_t element_size, unsigned long initial_size) {
+int vla_init(vla_t *vla, size_t element_size, unsigned long initial_capacity)
+{
+    if (element_size <= 0)
+        return VLA_ELEMENT_SIZE_ZERO;
+    
+    if (initial_capacity <= 0)
+        return VLA_ARRAY_SIZE_ZERO;
+
     vla->element_size = element_size;
     vla->size = 0;
-    vla->capacity = initial_size;
-    vla->elements = malloc(element_size * initial_size);
-    if (vla->elements == NULL) {
+    vla->capacity = initial_capacity;
+
+    vla->elements = malloc(element_size * initial_capacity);
+
+    if (vla->elements == NULL)
         return VLA_FAILURE;
-    }
+
     return VLA_SUCCESS;
 }
 
-int vla_push(vla_t *vla, void *element) {
-    if (vla->size == vla->capacity) {
-        if (vla_resize(vla, vla->capacity * 2) == VLA_FAILURE) {
+int vla_push(vla_t *vla, void *element)
+{
+    if (vla->size == vla->capacity)
+    {
+        if (vla_resize(vla, vla->capacity * 2) == VLA_FAILURE)
             return VLA_FAILURE;
-        }
     }
+
     memcpy(vla->elements + (vla->size * vla->element_size), element, vla->element_size);
     vla->size++;
+    
     return VLA_SUCCESS;
 }
 
-int vla_pop(vla_t *vla, void *element) {
-    if (vla->size == 0) {
-        return VLA_FAILURE;
-    }
+int vla_pop(vla_t *vla, void *element)
+{
+    if (vla->size == 0)
+        return VLA_ARRAY_SIZE_ZERO;
+
     memcpy(element, vla->elements[0], vla->element_size);
     vla->elements += vla->element_size;
-
     vla->size--;
+
     return VLA_SUCCESS;
 }
 
-int vla_get(vla_t *vla, unsigned long index, void *element) {
-    if (index >= vla->size) {
-        return VLA_FAILURE;
-    }
+int vla_get(vla_t *vla, unsigned long index, void *element)
+{
+    if (index >= vla->size)
+        return VLA_ARRAY_SIZE_MISMATCH;
     
     memcpy(element, vla->elements + (index * vla->element_size), vla->element_size);
+    
     return VLA_SUCCESS;
 }
 
-int vla_set(vla_t *vla, unsigned long index, void *element) {
-    if (index >= vla->size) {
-        return VLA_FAILURE;
-    }
+int vla_set(vla_t *vla, unsigned long index, void *element)
+{
+    if (index >= vla->size)
+        return VLA_ARRAY_SIZE_MISMATCH;
 
     memcpy(vla->elements + (index * vla->element_size), element, vla->element_size);
+    
     return VLA_SUCCESS;
 }
 
-int vla_delete(vla_t *vla, unsigned long index) {
-    if (index >= vla->size) {
-        return VLA_FAILURE;
-    }
+int vla_delete(vla_t *vla, unsigned long index)
+{
+    if (index >= vla->size)
+        return VLA_ARRAY_SIZE_MISMATCH;
 
-    vla->elements += (index * vla->element_size);
+    memmove(vla->elements + (index * vla->element_size), 
+            vla->elements + ((index + 1) * vla->element_size), 
+            (vla->size - index - 1) * vla->element_size);
+            
     vla->size--;
+
     return VLA_SUCCESS;
 }
 
-int vla_insert(vla_t *vla, void *element, unsigned long index) {
-    if (index > vla->size) {
-        return VLA_FAILURE;
-    }
-    if (vla->size == vla->capacity) {
-        if (vla_resize(vla, vla->capacity * 2) == VLA_FAILURE) {
+int vla_insert(vla_t *vla, void *element, unsigned long index)
+{
+    if (index > vla->size)
+        return VLA_ARRAY_SIZE_MISMATCH;
+
+    if (vla->size == vla->capacity)
+    {
+        if (vla_resize(vla, vla->capacity * 2) == VLA_FAILURE)
             return VLA_FAILURE;
-        }
     }
+
     memmove(vla->elements + (index * vla->element_size) + vla->element_size,
             vla->elements + (index * vla->element_size),
             (vla->size - index) * vla->element_size);
 
     vla->size++;
     memcpy(vla->elements + (index * vla->element_size), element, vla->element_size);
+    
     return VLA_SUCCESS;
 }
 
-int vla_clear(vla_t *vla) {
+int vla_clear(vla_t *vla)
+{
     vla->size = 0;
     return VLA_SUCCESS;
 }
