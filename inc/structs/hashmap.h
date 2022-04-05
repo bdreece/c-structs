0fc235f9e7d1dacecf2c3ad2a604c41cd8f35064
Brian Reece <bdreece@mtu.edu>
2022-04-05 01:25:12 -0400

    Refactored hashmap implementation

--- inc/structs/hashmap.h
+++ inc/structs/hashmap.h
@@ -1,4 +1,28 @@
 /**
+ * MIT License
+ *
+ * Copyright (c) 2022 Brian Reece
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
  * \file        hashmap.h
  * \brief       Hashmap data structure
  * \author      Brian Reece
@@ -75,13 +99,17 @@ STRUCTS_DEF bool hashmap_contains(const hashmap_t *map, const void *key);
  *  \param[out] val The value, if found
  *  \return Zero if successful, non-zero otherwise.
  */
-STRUCTS_DEF int hashmap_get(const hashmap_t *const map, const void *key, void *val);
+STRUCTS_DEF int hashmap_get(const hashmap_t *const map, const void *key,
+                            void *val);
 
-STRUCTS_DEF int hashmap_getp(const hashmap_t *const map, const void *key, void **val);
+STRUCTS_DEF int hashmap_getp(const hashmap_t *const map, const void *key,
+                             void **val);
 
-STRUCTS_DEF int hashmap_set(hashmap_t *const map, const void *key, const void *val);
+STRUCTS_DEF int hashmap_set(hashmap_t *const map, const void *key,
+                            const void *val);
 
-STRUCTS_DEF int hashmap_ins(hashmap_t *const map, const void *key, const void *val);
+STRUCTS_DEF int hashmap_ins(hashmap_t *const map, const void *key,
+                            const void *val);
 
 STRUCTS_DEF int hashmap_del(hashmap_t *const map, const void *key);
 
